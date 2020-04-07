9630a8552046f010463996b599689566186f85a9
Denis Fodor <fodor101@live.com>
2020-04-07 13:44:55 +0200

    Add logic for multiple transitions

--- app/src/main/java/com/dfodor/motionlayout/sample/MainActivity.kt
+++ app/src/main/java/com/dfodor/motionlayout/sample/MainActivity.kt
@@ -8,10 +8,25 @@ import androidx.constraintlayout.motion.widget.TransitionAdapter
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import com.dfodor.motionlayout.sample.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
 
 private const val SCALE = 1.2f
+private const val FIRST_ANIMATION_PART_DURATION = 400
+private const val SECOND_ANIMATION_PART_DURATION = 800
 
 class MainActivity : AppCompatActivity() {
+
+    private enum class StateSet {
+        START,
+        END,
+        FIRST_TO_SECOND,
+        SECOND_TO_THIRD,
+        FOURTH_TO_FIFTH,
+        SECOND_TO_FIRST,
+        THIRD_TO_SECOND,
+        FOURTH_TO_THIRD,
+        FIFTH_TO_FOURTH
+    }
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var animatedView: MotionLayout
@@ -25,6 +40,8 @@ class MainActivity : AppCompatActivity() {
     private val thirdSet = R.id.third_set
     private val fourthSet = R.id.fourth_set
     private val fifthSet = R.id.fifth_set
+
+    private var activeSet = StateSet.START
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,71 +58,101 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val transitionAdapter = object : TransitionAdapter() {
+
+        @Suppress("NON_EXHAUSTIVE_WHEN")
+        override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+            with(animatedView) {
+                when (activeSet) {
+                    StateSet.FIRST_TO_SECOND -> {
+                        setTransition(secondSet, thirdSet)
+                        setTransitionDuration(FIRST_ANIMATION_PART_DURATION)
+                        transitionToState(thirdSet)
+                        activeSet = StateSet.SECOND_TO_THIRD
+                    }
+                    StateSet.SECOND_TO_FIRST -> {
+                        binding.root.transitionToStart()
+
+                        val constraintSet = getConstraintSet(firstSet)
+                        constraintSet.setVisibility(R.id.thumbnail, ConstraintSet.GONE)
+                        constraintSet.applyTo(this)
+                        activeSet = StateSet.START
+                    }
+
+                    StateSet.THIRD_TO_SECOND -> {
+                        setTransition(secondSet, firstSet)
+                        setTransitionDuration(FIRST_ANIMATION_PART_DURATION)
+                        transitionToState(firstSet)
+                        activeSet = StateSet.SECOND_TO_FIRST
+                    }
+
+                    StateSet.FOURTH_TO_FIFTH -> {
+                        setTransition(fourthSet, fifthSet)
+                        setTransitionDuration(SECOND_ANIMATION_PART_DURATION)
+                        transitionToState(fifthSet)
+                        activeSet = StateSet.END
+                    }
+
+                    StateSet.FIFTH_TO_FOURTH -> {
+                        setTransition(fourthSet, thirdSet)
+                        setTransitionDuration(SECOND_ANIMATION_PART_DURATION)
+                        transitionToState(thirdSet)
+                        activeSet = StateSet.FOURTH_TO_THIRD
+                    }
+                }
+            }
+        }
+
+        override fun onTransitionStarted(motionLayout: MotionLayout, startId: Int, endId: Int) {
+            val progress = motionLayout.progress.roundToInt()
+            val isFourthSet = motionLayout.currentState == fourthSet
+            val isSecondSet = motionLayout.currentState == secondSet
+
+            if (isSecondSet && startId == secondSet) {
+                activeSet = StateSet.THIRD_TO_SECOND
+            } else if (isFourthSet && (progress == 0)) {
+                activeSet = StateSet.FOURTH_TO_FIFTH
+            } else if (isFourthSet && (progress == 1)) {
+                activeSet = StateSet.FIFTH_TO_FOURTH
+            }
+        }
+    }
+
+
     private fun animate(view: View, musicBand: MusicBand) {
         val marginTop = (view.height * SCALE).toInt() - (iconSize * SCALE / 2).toInt() - margin
 
-        var set = animatedView.getConstraintSet(firstSet)
-        set.setMargin(R.id.thumbnail, ConstraintSet.TOP, view.y.toInt())
-        set.setVisibility(R.id.thumbnail, ConstraintSet.VISIBLE)
-        set.setMargin(R.id.discography, ConstraintSet.TOP, marginTop)
-        set.applyTo(animatedView)
+        animatedView.also {
+            var set = it.getConstraintSet(firstSet)
+            set.setMargin(R.id.thumbnail, ConstraintSet.TOP, view.y.toInt())
+            set.setVisibility(R.id.thumbnail, ConstraintSet.VISIBLE)
+            set.setMargin(R.id.discography, ConstraintSet.TOP, marginTop)
+            set.applyTo(it)
 
-        set = animatedView.getConstraintSet(secondSet)
-        set.setMargin(R.id.discography, ConstraintSet.TOP, marginTop)
-        set.applyTo(animatedView)
+            set = it.getConstraintSet(secondSet)
+            set.setMargin(R.id.discography, ConstraintSet.TOP, marginTop)
+            set.applyTo(it)
 
-        set = animatedView.getConstraintSet(thirdSet)
-        set.setMargin(R.id.discography, ConstraintSet.TOP, marginTop)
-        set.applyTo(animatedView)
+            set = it.getConstraintSet(thirdSet)
+            set.setMargin(R.id.discography, ConstraintSet.TOP, marginTop)
+            set.applyTo(it)
 
-        with(binding) {
-            thumbnail.background =
-                ContextCompat.getDrawable(this@MainActivity, musicBand.drawableId)
-            name.text = musicBand.name
-            tags.text = musicBand.tags
-            description.text = musicBand.description
-            aboutText.text = musicBand.description
+            with(binding) {
+                thumbnail.background =
+                    ContextCompat.getDrawable(this@MainActivity, musicBand.drawableId)
+                name.text = musicBand.name
+                tags.text = musicBand.tags
+                description.text = musicBand.description
+                aboutText.text = musicBand.description
 
-            var started = false
-            animatedView.setTransitionListener(object : TransitionAdapter() {
-                override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                    when (currentId) {
-                        secondSet -> {
-                            animatedView.setTransition(secondSet, thirdSet)
-                            animatedView.setTransitionDuration(400)
-                            animatedView.transitionToState(thirdSet)
-                        }
-                        thirdSet -> {
-                            animatedView.setTransition(thirdSet, fourthSet)
-                            animatedView.setTransitionDuration(1000)
-                            animatedView.transitionToState(fourthSet)
-                        }
-                        fourthSet -> {
-                            // todo check flickering at the end of the animation
-                            animatedView.setTransition(fourthSet, fifthSet)
-                            animatedView.setTransitionDuration(1000)
-                            animatedView.transitionToState(fifthSet)
-                        }
-                    }
-                }
+                it.setTransitionListener(transitionAdapter)
 
-                override fun onTransitionStarted(
-                    motionLayout: MotionLayout,
-                    startId: Int,
-                    endId: Int
-                ) {
-//                    if (startId == firstSet) {
-//                        root.transitionToStart()
-//
-////                        val constraintSet = animatedView.getConstraintSet(firstSet)
-////                        constraintSet.setVisibility(R.id.thumbnail, ConstraintSet.GONE)
-////                        constraintSet.applyTo(animatedView)
-//                    }
-                }
-            })
-
-            root.transitionToEnd()
-            animatedView.transitionToEnd()
+                root.transitionToEnd()
+                it.setTransition(firstSet, secondSet)
+                it.setTransitionDuration(FIRST_ANIMATION_PART_DURATION)
+                it.transitionToState(secondSet)
+                activeSet = StateSet.FIRST_TO_SECOND
+            }
         }
     }
 }
